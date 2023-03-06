cb1a4be669f4876176a1c857effd74f261552102
Adammm <adammm.web@gmail.com>
2023-03-06 13:57:54 +0300

    create Project.tsx and added structure

---
+++ src/components/projects/project/Project.tsx
@@ -0,0 +1,16 @@
+import s from "./Project.module.css";
+import React from "react";
+
+export const Project = () => {
+    return (
+        <div className={s.itemProject}>
+            <div className={s.imgProject}>
+                <button>Смотреть</button>
+            </div>
+            <div className={s.description}>
+                <h6>title</h6>
+                <span>Описание</span>
+            </div>
+        </div>
+    )
+}

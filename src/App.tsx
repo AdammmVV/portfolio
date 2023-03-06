8bd799a01281940b3a1c93bf7122ca41090b61f0
Adammm <adammm.web@gmail.com>
2023-03-06 13:49:30 +0300

    create portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {Main} from "./components/main/Main";
+import {Skills} from "./components/skills/Skills";
+import {Projects} from "./components/projects/Projects";
+import {DistantWork} from "./components/distantWork/DistantWork";
+import {Form} from "./components/form/Form";
+import {Footer} from "./components/footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+
+            <Main/>
+            <Skills/>
+            <Projects/>
+            <DistantWork />
+            <Form />
+
+            <Footer />
+        </div>
+    );
 }
-
-export default App;

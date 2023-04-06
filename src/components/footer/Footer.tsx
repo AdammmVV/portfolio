8780dfee8a7be0959d450ce79b72bf26439ea557
Adammm <adammm.web@gmail.com>
2023-04-07 00:07:35 +0300

    refactor component Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,13 @@
 import React from "react";
-import s from "./Footer.module.css"
+import s from "./Footer.module.scss"
 import stylesContainer from '../../common/Container.module.css'
 
 export const Footer = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${stylesContainer.container} ${s.container}`}>
-                <h3>Адамчук Валерий</h3>
-                <div className={s.messengersWrapper}>
-                    <div className={s.messenger}>
-
-                    </div>
-                    <div className={s.messenger}>
-
-                    </div>
-                    <div className={s.messenger}>
-
-                    </div>
-                    <div className={s.messenger}>
-
-                    </div>
-                </div>
-                <span>© 2023 Все права защищены</span>
+                <h3>Adamchuk Valery</h3>
+                <span>Copyright © 2023 Incubator. All Rights Reserved.</span>
             </div>
         </div>
     )

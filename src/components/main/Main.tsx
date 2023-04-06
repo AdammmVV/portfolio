40a364115e8f0d2bfe8e9b798fe704d6fb1df03b
Adammm <adammm.web@gmail.com>
2023-04-07 00:07:24 +0300

    refactor component Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import s from './Main.module.css'
 import stylesContainer from '../../common/Container.module.css'
+import mainPhoto from './../../assets/image/avaMain.jpg'
+import {SuperButton} from "../../common/components/button/SuperButton";
 
 export const Main = () => {
+
+    const photo = {
+        backgroundImage: `url(${mainPhoto})`
+    }
+
     return (
-        <div className={s.main}>
+        <div className={s.main} id={'home'}>
             <div className={stylesContainer.container}>
                 <div className={s.description}>
-                    <span>Привет!</span>
-                    <h1>Меня зовут Валерий Адамчук.</h1>
-                    <p>Я front-end разработчик</p>
+                    <h2>Hi! My name is</h2>
+                    <h1>Valery <br/>Adamchuk</h1>
+                    <span>I`m Front-end developer</span>
+                    <div className={s.navigation}>
+                        <SuperButton name={'View My Works'}
+                                     variant={'primary'} />
+                        <SuperButton name={'Contact Me'}
+                                     variant={'link'}
+                                     icon={'arrowCircleDown'}/>
+                    </div>
                 </div>
-                <div className={s.mainPhoto}>
+                <div className={s.mainPhoto} style={photo}>
                 </div>
             </div>
+            <div className={s.arrow}/>
         </div>
     )
 }

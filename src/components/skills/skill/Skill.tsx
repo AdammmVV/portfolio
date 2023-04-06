9697d53cd0cb37dc4101b9cbfd85ae74e2b02e88
Adammm <adammm.web@gmail.com>
2023-04-07 00:06:36 +0300

    refactor component Skill.tsx

--- src/components/skills/skill/Skill.tsx
+++ src/components/skills/skill/Skill.tsx
@@ -1,18 +1,29 @@
-import s from "./Skill.module.css";
 import React from "react";
+import s from "./Skill.module.css";
 
-export const Skill = () => {
+type SkillPropsType = {
+    icon: { backgroundImage: string }
+    skillTitle: string
+    skillDescription: string
+}
+
+export const Skill: React.FC<SkillPropsType> = (
+    {
+        icon,
+        skillTitle,
+        skillDescription
+    }
+) => {
     return (
         <div className={s.skillItem}>
-            <div className={s.iconItem}>
-
+            <div className={s.iconItem} style={icon}>
             </div>
-            <div className={s.titleIcon}>
-                <p>React</p>
+            <div className={s.skillTitle}>
+                <p>{skillTitle}</p>
             </div>
-            <div className={s.description}>
-                <p>Подробное описание навыка</p>
-            </div>
+            {/*<div className={s.skillDescription}>*/}
+            {/*    <p>{skillDescription}</p>*/}
+            {/*</div>*/}
         </div>
     )
 }

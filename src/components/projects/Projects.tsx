ba4e9999dccc670c6c15f1c20a5afad8e478f9da
Adammm <adammm.web@gmail.com>
2023-04-07 00:06:50 +0300

    refactor component Projects.tsx

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import stylesContainer from '../../common/Container.module.css'
 import {Project} from "./project/Project";
+import {Title} from "../../common/components/title/Title";
 
 export const Projects = () => {
     return (
-        <div className={s.projectsBlock}>
-            <div className={`${stylesContainer.container} ${s.projectContainer}`}>
-                <h2>Мои Работы</h2>
+        <div className={s.projectsBlock} id={'projects'}>
+            <div className={`${stylesContainer.container} ${s.containerProjects}`}>
+                <Title mainTitle={'PORTFOLIO'} title={'My Works'}/>
                 <div className={s.projectsWrapper}>
                     <Project/>
                     <Project/>

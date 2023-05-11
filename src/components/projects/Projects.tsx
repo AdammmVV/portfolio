10b763ac4642680ac9129469b5fac7bc9ade9717
Adammm <adammm.web@gmail.com>
2023-05-11 16:34:53 +0300

    refactor: Profile

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,6 +3,7 @@ import s from './Projects.module.scss'
 import stylesContainer from '../../common/Container.module.css'
 import {Project} from "./project/Project";
 import {Title} from "../../common/components/title/Title";
+import {Fade} from "react-awesome-reveal";
 
 export const Projects = () => {
     return (
@@ -10,11 +11,12 @@ export const Projects = () => {
             <div className={`${stylesContainer.container} ${s.containerProjects}`}>
                 <Title mainTitle={'PORTFOLIO'} title={'My Works'}/>
                 <div className={s.projectsWrapper}>
-                    <Project/>
-                    <Project/>
+                    <Fade duration={1500} triggerOnce>
+                        <Project/>
+                        <Project/>
+                    </Fade>
                 </div>
             </div>
-
         </div>
     )
 }

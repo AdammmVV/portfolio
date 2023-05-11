88a0720efdcf69f56909beae2660327f9f94c76a
Adammm <adammm.web@gmail.com>
2023-05-11 16:34:07 +0300

    refactor: DistantWork

--- src/components/distantWork/DistantWork.tsx
+++ src/components/distantWork/DistantWork.tsx
@@ -3,13 +3,18 @@ import s from "./DistantWork.module.scss"
 import stylesContainer from '../../common/Container.module.css'
 import {SuperButton} from "../../common/components/button/SuperButton";
 import background from './../../assets/image/BacgroundImage/2.avif'
+import {Fade, Slide} from "react-awesome-reveal";
 
 export const DistantWork = () => {
     return (
         <div className={s.distantWorkBlock} style={{backgroundImage: `url(${background})`}}>
             <div className={`${stylesContainer.container} ${s.container}`}>
-                <h6>Considering remote work options</h6>
-                <SuperButton name={'Hire Me'} variant={'basic'} />
+                <Slide direction={'down'} triggerOnce>
+                    <h6>Considering remote work options</h6>
+                </Slide>
+                <Fade delay={500} triggerOnce>
+                    <SuperButton name={'Hire Me'} variant={'basic'}/>
+                </Fade>
             </div>
         </div>
     )

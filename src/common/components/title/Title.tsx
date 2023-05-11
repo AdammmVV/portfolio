92ffdfb202dafd363a13b4cd646e70f9c6f33269
Adammm <adammm.web@gmail.com>
2023-05-11 16:35:56 +0300

    refactor: Title

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import s from './Title.module.scss'
+import {Fade, Slide} from "react-awesome-reveal";
 
 type TitlePropsType = {
     mainTitle: string
     title: string
 }
 
-export const Title: React.FC<TitlePropsType> = ({
-    mainTitle,
-    title
-                                                }) => {
+export const Title: React.FC<TitlePropsType> =
+    ({mainTitle, title}) => {
     return (
-        <div className={s.title}>
-            <h2>{mainTitle}</h2>
-            <p>{title}</p>
+        <div className={s.titleWrapper}>
+            <Slide className={s.mainTitle} direction={'down'}>
+                <h2>{mainTitle}</h2>
+            </Slide>
+            <Fade className={s.title} delay={200}>
+                <p>{title}</p>
+            </Fade>
         </div>
     )
 }

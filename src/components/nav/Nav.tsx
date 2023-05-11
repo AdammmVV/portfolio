7f290ab686b7fdcb3ef1a99705f8d43d342bcafc
Adammm <adammm.web@gmail.com>
2023-05-11 16:34:42 +0300

    refactor: Nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,13 +1,45 @@
 import React from "react";
 import s from './Nav.module.scss'
+import {Link} from "react-scroll";
 
 export const Nav = () => {
     return (
-        <div className={s.nav}>
-            <a href="#home">Home</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
+        <div className={s.navWrapper}>
+            <div className={s.nav}>
+                <Link activeClass={s.active}
+                      to='home'
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={500}>
+                    Home
+                </Link>
+                <Link activeClass={s.active}
+                      to="skills"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={500}>
+                    Skills
+                </Link>
+                <Link activeClass={s.active}
+                      to="projects"
+                      spy={true}
+                      smooth={true}
+                      offset={-71}
+                      duration={500}>
+                    Projects
+                </Link>
+                <Link activeClass={s.active}
+                      to="contact"
+                      spy={true}
+                      smooth={true}
+                      offset={-110}
+                      duration={500}>
+                    Contact
+                </Link>
+            </div>
         </div>
+
     )
 }

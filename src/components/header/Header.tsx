ec10a8694d3367a4706d85f500c822d1ec78c390
Adammm <adammm.web@gmail.com>
2023-05-11 16:34:15 +0300

    refactor: Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Nav} from "../nav/Nav";
 import s from './Header.module.scss'
+import {BurgerNav} from "../burgerNav/BurgerNav";
 
 export const Header = () => {
+    const [activeBurger, setActiveBurger] = useState(false)
+    const onActiveBurger = () => {
+        setActiveBurger(!activeBurger)
+    }
     return (
         <div className={s.header}>
             <Nav/>
+            <BurgerNav activeBurger={activeBurger} onActiveBurger={onActiveBurger}/>
         </div>
     )
 }

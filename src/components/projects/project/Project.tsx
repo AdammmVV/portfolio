7c48a2c246f227862cafdb5204c9ec7fde15e0dc
Adammm <adammm.web@gmail.com>
2023-06-17 18:10:46 +0300

    refactor: Project

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,17 +1,23 @@
 import s from "./Project.module.scss";
 import React from "react";
 import {SuperButton} from "../../../common/components/button/SuperButton";
-import socialNetwork from './../../../assets/image/IconProject/connected-world-concept-illustration_114360-7523.avif'
 
-export const Project = () => {
+type ProjectPropsType = {
+    title: string
+    href: string
+    img: string
+    description: string
+}
+
+export const Project: React.FC<ProjectPropsType> = ({ title, img, description, href}) => {
     return (
         <div className={s.itemProject}>
-            <div className={s.imgContainer} style={{backgroundImage: `url(${socialNetwork})`}}>
-                <SuperButton name={'Watch'} variant={'secondary'} />
+            <div className={s.imgContainer} style={{backgroundImage: `url(${img})`}}>
+                <SuperButton name={'Watch'} variant={'secondary'} href={href}/>
             </div>
             <div className={s.description}>
-                <h6>Social Network</h6>
-                <span>Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt earum eius explicabo fuga illo impedit maxime modi mollitia optio possimus quae quaerat quasi quis, ratione repellat suscipit temporibus totam vero?</span>
+                <h6>{title}</h6>
+                <span>{description}</span>
             </div>
         </div>
     )

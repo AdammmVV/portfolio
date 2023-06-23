35cb99c1427453ccc304b3a5867a3115f1bbe93f
Adammm <adammm.web@gmail.com>
2023-06-23 19:33:54 +0300

    update: download CV

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,27 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://gmail-nodejs-beige.vercel.app/',
+    baseURL: 'http://localhost:3010/',
 })
 
 export const api = {
     sendMessage: (data: { name: string, email: string, description: string }) => {
-        return instance.post<{message: string}>('sendMessage', data)
+        return instance.post<{ message: string }>('sendMessage', data)
+    },
+    downloadCV: () => {
+        return instance.get('download', {responseType: 'blob'}).then(({data}) => {
+            const blob = new Blob(
+                [data],
+                {type: 'application/pdf'}
+            )
+            const downloadUrl = window.URL.createObjectURL(blob)
+
+            const link = document.createElement('a')
+            link.href = downloadUrl
+            link.setAttribute('download', 'AdamchukValery(ru_eng).pdf')
+            document.body.appendChild(link)
+            link.click()
+            link.remove()
+        })
     }
 }
